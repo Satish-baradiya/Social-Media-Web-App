44aa71c83d35806787db7b85a327a4e765182694
Satish-baradiya <baradiyasatish@gmail.com>
2022-01-13 21:32:13 +0530

    Extending admin

--- switter/admin.py
+++ switter/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from django.contrib.auth import models
+from django.contrib.auth.models import Group, User
+from django.db.models import fields
 # Register your models here.
+from . models import Profile
+
+admin.site.register(Profile)
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
