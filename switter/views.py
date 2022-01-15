c9b9b618984e633e6519df209e95013b254e0c9c
Satish-baradiya <baradiyasatish@gmail.com>
2022-01-15 20:07:27 +0530

    Views added

--- switter/views.py
+++ switter/views.py
@@ -1,3 +1,14 @@
+import profile
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
+
+def dashboard(request):
+    return render(request,'base.html')
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request,"switter/profile_list.html",{
+        "profiles":profiles
+    })
