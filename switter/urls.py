5e796b2675f8913ffac138d5f5431aab4898b816
Satish-baradiya <baradiyasatish@gmail.com>
2022-01-16 19:18:09 +0530

    Updates in views

--- switter/urls.py
+++ switter/urls.py
@@ -1,9 +1,10 @@
+import profile
 from unicodedata import name
 from . import views
 from django.urls import path
 
 urlpatterns = [ 
     path("",views.dashboard,name="dashboard"),
-    path("profile_list/",views.profile_list,name="profile_list")
-    
+    path("profile_list/",views.profile_list,name="profile_list"),
+    path("profile/<int:pk>",views.profile,name="profile")
 ]
